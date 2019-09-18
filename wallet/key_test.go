d6a41dc45053ea313a3dcb3fa394b99cbb8f7ff9
klim0v <klim0v-sergey@yandex.ru>
2019-09-18 17:02:02 +0300

    wip: add AddressByPublicKey

--- wallet/key_test.go
+++ wallet/key_test.go
@@ -8,11 +8,8 @@ import (
 const (
 	validSeed       = "33fa1096997d9b0f47469463710b3a2e91971144265b281dc71f831539a3b8e3413e5969e5ffb4d3c5a37cfa0f964bcc779efe4ae37fceef048175105caad624"
 	validPrivateKey = "d3520cc797f12b8a81e805ddf5a5bf8b994e347003ea25c9ccaecb5073f3fef1"
+	validPublicKey  = "Mp12a4d537e4b8bc98a99537a130a5d871536762bb8505a4935edc75fef97b1205977fffb0b3dbe92b7a18be8bd2068c9d729dab87cd1d76ac4522b58328da832d"
 	validAddress    = "Mx48f502a9fc324f2c707edc3a2595e72f00c3190c"
-
-	validPrivateKey2 = "21bdd69f4a6d0db9508bc543e26bd23378518c8c409496f9ef3e015ff17fc005"
-	validPublicKey2  = "Mpb03a4a53b02fba4023ed1afd2598fa1e5fae220198109b865218598f9456a3be4a1e577e0ddb8c72c55b3a0e7102eb35c9dccc1dd4e32228a369b0a57e15bddf"
-	validAddress2    = "Mx17b1240ba6d45258f836b45ae0c4fc1106f5ce59"
 )
 
 func TestSeed(t *testing.T) {
@@ -60,22 +57,23 @@ func TestAddress(t *testing.T) {
 }
 
 func TestPublicKeyByPrivateKey(t *testing.T) {
-	pubKey, err := PublicKeyByPrivateKey(validPrivateKey2)
+	pubKey, err := PublicKeyByPrivateKey(validPrivateKey)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if pubKey != validPublicKey2 {
-		t.Fatalf("PublicKey got %s, want %s", pubKey, validPublicKey2)
+	if pubKey != validPublicKey {
+		t.Fatalf("PublicKey got %s, want %s", pubKey, validPublicKey)
 	}
 }
 
 func TestAddressByPublicKey(t *testing.T) {
-	address, err := AddressByPublicKey(validPublicKey2)
+	address, err := AddressByPublicKey(validPublicKey)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if address != validAddress2 {
-		t.Fatalf("Address got %s, want %s", address, validAddress2)
+
+	if address != validAddress {
+		t.Fatalf("Address got %s, want %s", address, validAddress)
 	}
 }
