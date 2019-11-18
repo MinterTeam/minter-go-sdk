43def5baf7cb40d71480d516daeecae553dfc5e1
klim0v <klim0v-sergey@yandex.ru>
2019-11-18 13:14:07 +0300

    fix test

--- transaction/deeplink_test.go
+++ transaction/deeplink_test.go
@@ -10,7 +10,7 @@ func TestDeepLink_NewDeepLink(t *testing.T) {
 		NewSendData().
 			MustSetTo("Mx18467bbb64a8edf890201d526c35957d82be3d95").
 			SetCoin("BIP").
-			SetValue(big.NewInt(0).Mul(big.NewInt(12345), big.NewInt(0).Exp(big.NewInt(10), big.NewInt(14), nil))),
+			SetValue(big.NewInt(0).Mul(big.NewInt(123456789), big.NewInt(0).Exp(big.NewInt(10), big.NewInt(18-8), nil))),
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -24,7 +24,7 @@ func TestDeepLink_NewDeepLink(t *testing.T) {
 	}
 
 	data := "f83b01aae98a424950000000000000009418467bbb64a8edf890201d526c35957d82be3d9588112210f4768db4008b48656c6c6f20576f726c64808080"
-	if encode == data {
+	if encode != data {
 		t.Errorf("Encode got %s, want %s", encode, data)
 	}
 }
