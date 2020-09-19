b890bb9b4607690d214cf5197812b2281844c77b
klim0v <klim0v-sergey@yandex.ru>
2020-09-20 00:42:42 +0300

    basePath v2

--- api/http_client/client/minter_node_api_v2_client.go
+++ api/http_client/client/minter_node_api_v2_client.go
@@ -22,7 +22,7 @@ const (
 	DefaultHost string = "localhost"
 	// DefaultBasePath is the default BasePath
 	// found in Meta (info) section of spec file
-	DefaultBasePath string = "/"
+	DefaultBasePath string = "/v2"
 )
 
 // DefaultSchemes are the default schemes found in Meta (info) section of spec file
