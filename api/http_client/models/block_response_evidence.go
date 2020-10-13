7e4b6eba091ae89c72526c4778e3af074704596c
klim0v <klim0v-sergey@yandex.ru>
2020-10-13 19:09:55 +0300

    add evidence

--- api/http_client/models/block_response_evidence.go
+++ api/http_client/models/block_response_evidence.go
@@ -6,9 +6,6 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	"strconv"
-
-	"github.com/go-openapi/errors"
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -19,45 +16,11 @@ import (
 type BlockResponseEvidence struct {
 
 	// evidence
-	Evidence []*BlockResponseEvidenceEvidence `json:"evidence"`
+	Evidence []interface{} `json:"evidence"`
 }
 
 // Validate validates this block response evidence
 func (m *BlockResponseEvidence) Validate(formats strfmt.Registry) error {
-	var res []error
-
-	if err := m.validateEvidence(formats); err != nil {
-		res = append(res, err)
-	}
-
-	if len(res) > 0 {
-		return errors.CompositeValidationError(res...)
-	}
-	return nil
-}
-
-func (m *BlockResponseEvidence) validateEvidence(formats strfmt.Registry) error {
-
-	if swag.IsZero(m.Evidence) { // not required
-		return nil
-	}
-
-	for i := 0; i < len(m.Evidence); i++ {
-		if swag.IsZero(m.Evidence[i]) { // not required
-			continue
-		}
-
-		if m.Evidence[i] != nil {
-			if err := m.Evidence[i].Validate(formats); err != nil {
-				if ve, ok := err.(*errors.Validation); ok {
-					return ve.ValidateName("evidence" + "." + strconv.Itoa(i))
-				}
-				return err
-			}
-		}
-
-	}
-
 	return nil
 }
 
