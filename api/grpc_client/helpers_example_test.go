6496659a97f110b4f11e06af1b22e911240f8f16
klim0v <klim0v-sergey@yandex.ru>
2020-10-17 02:33:33 +0300

    example

--- api/grpc_client/helpers_example_test.go
+++ api/grpc_client/helpers_example_test.go
@@ -1,8 +1,16 @@
 package grpc_client_test
 
 import (
+	"github.com/MinterTeam/minter-go-sdk/v2/api"
 	"github.com/MinterTeam/minter-go-sdk/v2/api/grpc_client"
 )
+
+func mark(address, publicKey string) {}
+
+func doSomething1(*api.RewardEvent)    {}
+func doSomething2(*api.SlashEvent)     {}
+func doSomething3(*api.UnbondEvent)    {}
+func doSomething4(*api.StakeKickEvent) {}
 
 func ExampleConvertStructToEvent() {
 	client, _ := grpc_client.New("localhost:8842")
@@ -10,7 +18,18 @@ func ExampleConvertStructToEvent() {
 	for _, data := range events.Events {
 		event, _ := grpc_client.ConvertStructToEvent(data)
 
-		_ = event.GetAddress()
-		_ = event.GetValidatorPublicKey()
+		mark(event.GetAddress(), event.GetValidatorPublicKey())
+
+		switch e := event.(type) {
+		case *api.RewardEvent:
+			doSomething1(e)
+		case *api.SlashEvent:
+			doSomething2(e)
+		case *api.UnbondEvent:
+			doSomething3(e)
+		case *api.StakeKickEvent:
+			doSomething4(e)
+		}
+
 	}
 }
